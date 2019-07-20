3082154cddab91411567b40e022350ccbb056808
iitsliiz <emtuitt@gmail.com>
2019-07-20 16:26:13 -0400

    incomplete includes map err

---
+++ src/Components/App/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {
+        searchResults: [],
+        playlistName: '',
+        playlistTracks: []
+      }
+    
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+
+  }
+
+  addTrack(track){
+      if(this.state.playlistTracks.find(savedTrack =>
+        savedTrack.id === track.id
+      )) {
+        return;
+      }
+  }
+
+  removeTrack(track){
+    let newPlaylist = [];
+    newPlaylist = this.state.playlistName.filter(savedTrack => {
+      return savedTrack.id !== track.id
+    })
+    this.setState({playlist: newPlaylist})
+    }
+      
+
+     updatePlaylistName(name){
+      this.setState({
+          playlistName: name
+      })
+     }
+
+     savePlaylist(){
+      const trackURIs = this.state.playlistTracks.map(track => track.uri);
+     }
+
+     search(term){
+      console.log(term);
+     }
+
+    render(){
+      return (
+        <div>
+           <h1>Ja<span className="highlight">mmm</span>ing</h1>
+           < div className="App">
+           <SearchBar onSearch={this.state.search} />
+           <div className="App-playlist">
+           <SearchResults searchResults={this.state.searchResults} onAdd={this.state.addTrack}/>
+           <Playlist  onSave={this.state.savePlaylist} onNameChange={this.state.updatePlaylistName} onRemove={this.state.removeTrack} playnameList={this.state.playlistName} playlistTracks={this.playlistTracks}/>
+        </div>
+    </div>
+    </div>
+  );
+  }
+}
+
+export default App;
